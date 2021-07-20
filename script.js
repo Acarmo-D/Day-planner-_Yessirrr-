64c19f04f4721a4009777148ad074c4f6e3030e0
monsieur <alex.carmody@hotmail.com>
2021-07-19 19:56:21 -0700

    updated java

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
-var Timeblock = $('Time-Blocks')
-var Savebtn = $('saveBtn')
-var Happend = $('past')
-var After = $('Future')
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    
+    var arrayTimeBlock = document.querySelectorAll(".time-block");
+    var currentTime = moment().hour();
+    arrayTimeBlock.forEach(function(timeblock) {
+        var timeBlockId = $(timeblock).attr("hour");
+        if (timeBlockId < currentTime) {
+            $(timeblock).addClass("past");
+        } else if (timeBlockId > currentTime) {
+            $(timeblock).addClass("future");
+        }  else if (timeBlockId == currentTime) {
+            $(timeblock).addClass("present");
+        }
+    });
+
+    // var arraySaveBtn = document.querySelectorAll(".save-btn");
+    // arraySaveBtn.forEach(function(saveBtn) {
+    //     $(saveBtn)
+    // });
+
+    $(".container").on("click",".save-btn",function(event) {
+        //get the hour id
+        var id = $(event.target).closest(".time-block").attr("hour");
+        //get the text area input
+        var text = $(event.target).closest(".time-block").find(".text").val();
+        //create an obj entry
+        var dataEntry = {
+            id: id,
+            text: text
+        };
+        //get the old data
+        var data  = JSON.parse(localStorage.getItem("data")) || [];
+        //update the old data
+        data.push(dataEntry)
+        //strore the updated data
+        localStorage.setItem("data",JSON.stringify(data));
+    });
+
+    var data  = JSON.parse(localStorage.getItem("data")) || [];
+    data.forEach(function(datum) {
+        var queryString = "[hour='"+datum.id+"']";
+        $(queryString).find(".text").val(datum.text)
+    });
+});
